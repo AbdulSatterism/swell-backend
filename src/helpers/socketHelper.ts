c4110122f11b78ca765e3c95d3dd7f1efcbdc45e
AbdulSatterism <abdulsatter.ism@gmail.com>
2024-12-28 22:41:19 -0800

    update socket helper

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -4,6 +4,7 @@ import { Server } from 'socket.io';
 import { logger } from '../shared/logger';
 
 const socket = (io: Server) => {
+  /*
   io.on('connection', socket => {
     console.log('A user connected:', socket.id);
 
@@ -32,6 +33,40 @@ const socket = (io: Server) => {
     });
 
     //disconnect
+    socket.on('disconnect', () => {
+      logger.info(colors.red('A user disconnect'));
+    });
+  });
+
+  */
+
+  io.on('connection', socket => {
+    console.log('A user connected:', socket.id);
+
+    // Join a chat room
+    socket.on('join', roomId => {
+      socket.join(roomId);
+      console.log(`User joined room: ${roomId}`);
+    });
+
+    // Listen for new messages and emit to the specific chat room
+    socket.on('new-message', ({ chatRoom, senderId, message }) => {
+      // Emit message to the specific chat room
+      io.to(chatRoom).emit(`new-message:${chatRoom}`, {
+        senderId,
+        message,
+      });
+    });
+
+    // Listen for the chat-started event and emit to the specific room
+    socket.on('chat-started', ({ chatRoom }) => {
+      io.to(chatRoom).emit(`chat-started:${chatRoom}`, {
+        chatRoom,
+        message: 'Chat started between the groups.',
+      });
+    });
+
+    // Handle disconnection
     socket.on('disconnect', () => {
       logger.info(colors.red('A user disconnect'));
     });
